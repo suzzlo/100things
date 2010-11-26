56ece7290f8a6dc3b86dc12cec16429b3c9cb607
Ion Gonzalez <ion@suzzlo.org>
2010-11-26 23:13:29 +0100

    Add changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Hundredthings::Application.routes.draw do
-  resources :categories
 
+  devise_for :users, :path_names => { :sign_up => "register" }
+
+  resources :things, :categories
+  
+  root :to => "things#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -57,4 +62,5 @@ Hundredthings::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  
 end
